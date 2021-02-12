f8bf094a9294a666a9ec5477c99fe8bc2cc6b12d
Dylan <02dylan.despins@gulllakecs.org>
2021-02-12 18:09:09 -0500

    Price TextView and Variable

--- app/src/main/java/com/example/justjava20_21/MainActivity.java
+++ app/src/main/java/com/example/justjava20_21/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
@@ -17,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int numberOfCoffees = 3;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
     }
+
+
+
+
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
